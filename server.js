516aed60f2496b4fb3e640493718612b0d732d7f
Cameron Roman <camroman95@gmail.com>
2018-11-11 05:18:42 -0500

    Updated Version 1

--- server.js
+++ server.js
@@ -24,12 +24,12 @@ mongoose
 app.use('/api/items', items);
 
 // Serve static assets if in production
-if(process.env.Node_ENV === 'production') {
+if(process.env.NODE_ENV === 'production') {
     //Set static folder
     app.use(express.static('client/build'));
 
-    app.get('*', (req,res) =>{
-        res.sendFile(path.resolve(__dirname, 'client', 'build', index.html));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
 
